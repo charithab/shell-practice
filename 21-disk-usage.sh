cf1030ab48ce804bea168b534a0714ae0841ef87
charitha <charitha.harinath@gmail.com>
2025-07-30 11:44:40 +0530

    shell practice

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -7,8 +7,8 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
-    USAGE=$(echo line | awk "{print $6F}" | cut -d "%" -f1)
-    PARTITION=$(echo line | awk "{print $7F}")
+    USAGE=$(echo $line | awk "{print $6F}" | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk "{print $7F}")
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "High Disk Usage on $PARTITION: $USAGE % <br>"
